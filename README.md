46bfd2d10d27b1f69ca0555b5b7f73212f8dd0d0
John H. Robinson, IV <jaqque@gmail.com>
2020-08-07 17:22:01 -0700

    Initial Import

---
+++ README.md
@@ -0,0 +1,18 @@
+# Bresenham’s Circle Drawing Algorithm
+
+Useful for plotting mostly unique points of a circle on a graph.
+
+## Usage
+
+`./circle.sh`
+
+## Options
+
+- `-x <int>` Define circle's X center point (Default: 0)
+- `-y <int>` Define circle's Y center point (Default: 0)
+- `-r <int>` Define circle's radius (Default: 100)
+- `-f <string>` Define output format (Default: "(%d,%d)\n")
+
+## Bugs
+
+There is no error checking. Pull requests welcome.
---
+++ circle.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Bresenham's Circle
+# Totally cribbed from https://iq.opengenus.org/bresenhams-circle-drawing-algorithm/
+
+plot() {
+   # Look ma! Globals!
+   printf "$format" $(( Xc + x )) $(( Yc + y ))
+   printf "$format" $(( Xc + x )) $(( Yc - y ))
+   printf "$format" $(( Xc - x )) $(( Yc + y ))
+   printf "$format" $(( Xc - x )) $(( Yc - y ))
+   printf "$format" $(( Xc + y )) $(( Yc + x ))
+   printf "$format" $(( Xc + y )) $(( Yc - x ))
+   printf "$format" $(( Xc - y )) $(( Yc + x ))
+   printf "$format" $(( Xc - y )) $(( Yc - x ))
+}
+
+
+while [ "$*" ]; do
+   case "$1" in
+      # Look ma! No error checking!
+      -r) R="$2";  shift; shift ;;
+      -x) Xc="$2"; shift; shift ;;
+      -y) Yc="$2"; shift; shift ;;
+      -f) format="$2"; shift; shift ;;
+      -h|--help) echo "No help"; exit ;;
+      *) echo "Use --help. You need it"; exit 1 ;;
+   esac
+done
+
+[ "$R" ]  || R=100
+[ "$Xc" ] || Xc=0
+[ "$Yc" ] || Yc=0
+[ "$format" ] || format='(%d,%d)\n'
+
+x=0
+y=$(( R ))
+d=$(( 3 - 2 * r ))
+
+plot
+while [ $y -ge $x ]; do
+   x=$(( x+1 ))
+   if [ $d -gt 0 ]; then
+      y=$(( y-1 ))
+      d=$(( d + 4 * (x - y) + 10 ))
+   else
+      d=$(( d + 4 * x + 6 ))
+   fi
+   plot
+done
